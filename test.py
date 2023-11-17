0362f69949e6bcef18c8d962cab0dbf42d3d28eb
LaiChengTing <zseff123@gmail.com>
2023-11-17 18:11:24 +0800

    push

---
+++ test.py
@@ -0,0 +1,10 @@
+from scheme import *
+# expr = read_line('(+ 2 2)')
+# scheme_eval(expr, create_global_frame())
+# print(type(expr))
+
+# pro  = BuiltinProcedure("haha", lambda s:abs(s))
+# print(check_procedure(pro))
+# expr = read_line("('a)")
+# scheme_eval(expr.second.second, create_global_frame())
+# print(LambdaProcedure(Pair("x", Pair("y", nil)), Pair("+", Pair("x", Pair("y", nil))), make_))
